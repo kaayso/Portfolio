51c2f2a3525ad1c879a58dadf45b0e37fc26ae3e
Faycel Benyoussa <kaayso21@gmail.com>
2019-12-19 21:28:53 +0100

    Initial commit from Create React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import { AccessAlarm, ThreeDRotation, Delete } from "@material-ui/icons";
+import NavBar from "components/navBar/navBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navBar/navBar.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import "./navBar.css";
+
+export default function NavBar() {
+  return <div className="navBar">ssddds</div>;
+}
