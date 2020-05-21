4c21daf706bcb6ff1964a29d43d5c3479cb95f3e
Faycel Benyoussa <faysou@laposte.net>
2020-05-21 05:14:21 +0200

    to top btn

--- src/components/toTopBtn/toTopBtn.jsx
+++ src/components/toTopBtn/toTopBtn.jsx
@@ -2,10 +2,32 @@ import React from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import './toTopBtn.css';
+import { useWindowScroll } from 'react-use';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 export default function ToTopBtn() {
+  const [isMount, setIsMount] = React.useState(false);
+  React.useEffect(() => {
+    setIsMount(true);
+  }, []);
+  const mobileBreakPoint = useMediaQuery('(max-width:959px)');
+  const { y } = useWindowScroll();
+
+  const toTopBtnVisible = !mobileBreakPoint && y > 1500 && isMount;
+
+  if (document.getElementById('toTopBtn')) {
+    if (toTopBtnVisible) {
+      document.getElementById('toTopBtn').className =
+        'MuiButtonBase-root MuiIconButton-root toTopBtn toTopBtn-slide-from-right';
+    } else {
+      console.log('up');
+
+      document.getElementById('toTopBtn').className =
+        'MuiButtonBase-root MuiIconButton-root toTopBtn toTopBtn-slide-right';
+    }
+  }
   return (
-    <IconButton href="#app-root" className="toTopBtn ">
+    <IconButton href="#app-root" className="toTopBtn" id="toTopBtn">
       <KeyboardArrowUpIcon />
     </IconButton>
   );
--- src/containers/home/home.jsx
+++ src/containers/home/home.jsx
@@ -5,45 +5,10 @@ import Cv from 'containers/cv/cv';
 import Projects from 'containers/projects/projects';
 import Route from 'containers/route/route';
 import Skills from 'containers/skills/skills';
-import { gsap } from 'gsap';
-import { useIntersection } from 'react-use';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 export default function Home() {
-  const mobileBreakPoint = useMediaQuery('(max-width:959px)');
-  const myRef = React.useRef(null);
-  const THRESHOLD = 0.4;
-
-  const intersection = useIntersection(myRef, {
-    root: null,
-    rootMargin: '0px',
-    threshold: THRESHOLD,
-  });
-
-  const animationsIn = (element1) => {
-    console.log('animationsIn');
-    gsap.to(element1, 1, {
-      ease: 'elastic',
-      x: 0,
-      opacity: 1,
-    });
-  };
-  const animationsOut = (element1) => {
-    console.log('animationsOut');
-    console.log(intersection && intersection.intersectionRatio);
-
-    gsap.to(element1, 1, {
-      x: 80,
-      ease: 'power4.out',
-      opacity: 1,
-    });
-  };
-
-  intersection && intersection.intersectionRatio > THRESHOLD
-    ? animationsIn('.toTopBtn')
-    : animationsOut('.toTopBtn');
   return (
-    <div ref={myRef}>
+    <div>
       <Introduction />
       <Development />
       <Cv />
